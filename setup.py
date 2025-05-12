ee31be15b706c83e51a6c4f67ddfe306bf70aecc
Cyfraice <140945337+Cyfraice@users.noreply.github.com>
2025-05-12 14:15:36 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yapsy',
+    version='1.12.2-py312',
+    description='Fat-free DIY Python plugin management toolkit (patched for Python 3.12+)',
+    author='Talossec (fork of Olivier Guilyardi)',
+    url='https://github.com/talos-security/yapsy',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
